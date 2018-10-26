032531a31558ca21533af996929d8ef138d360de
rblm1107 <rbelmustapha@gmail.com>
2018-10-26 13:10:22 +0200

    modify

---
+++ fichier.js
@@ -0,0 +1,7 @@
+
+var first = document.getElementById('first');
+
+console.log("cet element a "+first.childNodes.length + " noeuds enfants");
+var paragraphe = document.getElementsByTagName('p');
+console.log(paragraphe);
+console.log("il y a "+paragraphe.length+" paragraphes dans cette page");
